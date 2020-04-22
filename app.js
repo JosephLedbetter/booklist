6aca45ea0c592ce345ac364cbd51be5e6f930fc3
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-04-22 19:18:29 -0400

    appending a list element to the book list holder

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ function Book(title, author, isbn){
 // UI Constructor
 function UI() {}
 
+UI.prototype.addBookToList = function(book){
+const list = document.getElementById('book-list');
+// creating tr element
+const row = document.createElement('tr');
+// inserting cols
+row.innerHTML = `
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td>${book.isbn}</td>
+<td><a href="#" class="delete">X</a></td>
+`;
+list.appendChild(row);
+}
+
 // Event listeners
 document.querySelector('.book-form').addEventListener('submit', function(e){
 const  title = document.querySelector('#title').value,
@@ -15,7 +29,14 @@ const  title = document.querySelector('#title').value,
         isbn = document.querySelector('#isbn').value;
 // Instantiating a new book
 const book = new Book(title, author, isbn);
-console.log(book)
+
+// instantiating a new ui object
+const ui = new UI();
+
+// adding a book to the list
+ui.addBookToList(book)
+ 
+
     e.preventDefault()
 });
 
